248085d0146d709eb5ea595f362e368bbb7ba5bc
Sewonzzang123 <dkrlrhdrur@gmail.com>
2021-07-21 12:11:11 +0900

    README

--- basic/ex01/dev-server.js
+++ basic/ex01/dev-server.js
@@ -1,7 +1,7 @@
 import http from "http";
 import express from "express";
 import * as path from "path";
-const port = 6000;
+const port = 8080;
 
 const application = express().use(
   express.static(path.join(path.resolve("."), "public"))
