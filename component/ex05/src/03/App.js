dba1c2559dac6451eaf85475ce4ce40e1f5711b1
Sewonzzang123 <dkrlrhdrur@gmail.com>
2021-08-06 12:53:40 +0900

    clock class component

--- component/ex05/src/03/App.js
+++ component/ex05/src/03/App.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
-import './assets/scss/App.scss'
-import Clock from './Clock';
+import React, { Component } from "react";
+import "./assets/scss/App.scss";
+import Clock from "./Clock";
 
 export default class App extends Component {
-    constructor() {
-        super(...arguments);
-    }
+  constructor() {
+    super(...arguments);
+    this.state = {
+      showClock: true,
+    };
+  }
 
-    render() {
-        return (
-            <div className='clock-display'>
-                <h2>ex05 - Component LifeCycle Practice</h2>
-                    <Clock />
-            </div>
-        );
-    }
+  hideClock() {
+    this.setState({ showClock: false });
+  }
+
+  render() {
+    return (
+      <div className="clock-display">
+        <h2>ex05 - Component LifeCycle Practice</h2>
+        {this.state.showClock ? (
+          <Clock callback={this.hideClock.bind(this)} />
+        ) : null}
+      </div>
+    );
+  }
 }
